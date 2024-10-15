62f0d562520ae3aa3841e1a25012acdaa98046e1
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-10-15 00:40:36 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,9 @@
 def leap_year():
-    year = int(input("Ingrese un año: "))
-    if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
-        print(f"El año {year} es bisiesto")
-    else:
-        print(f"El año {year} no es bisiesto") 
+  year = int(float(input("")))
+    if year % 4 == 0 and year % 100 == 0:
+        if year % 400 == 0:
+            print(f"El año {year} es bisiesto")
+        else: 
+            print(f"El año {year} no es bisiesto")
+    else: 
+        print(f"El año {year} no es bisiesto")  
